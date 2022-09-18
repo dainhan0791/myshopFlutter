3954838feaf133eb1431761d0afd2a4b5326d92a
Thai Dai Nhan <thaidainhanfb792001@gmail.com>
2022-09-18 21:04:23 +0700

    Xay dung trang cac dat hang

---
+++ lib/ui/orders/orders_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/models/order_item.dart';
+
+import 'orders_manager.dart';
+import 'order_item_card.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('building orders');
+    final ordersManager = OrdersManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: ordersManager.orderCount,
+        itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+      ),
+    );
+  }
+}
