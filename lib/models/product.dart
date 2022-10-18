f362303d671333fc275d02ebfdab36adee0bf5b6
Thai Dai Nhan <thaidainhanfb792001@gmail.com>
2022-10-18 12:00:13 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/foundation.dart';
+
 class Product {
   final String? id;
   final String title;
   final String description;
   final double price;
   final String imageUrl;
-  final bool isFavorite;
+  // final bool isFavorite;
+  final ValueNotifier<bool> _isFavorite;
 
   Product({
     this.id,
@@ -12,8 +15,8 @@ class Product {
     required this.description,
     required this.price,
     required this.imageUrl,
-    this.isFavorite = false,
-  });
+    isFavorite = false,
+  }) : _isFavorite = ValueNotifier(isFavorite);
 
   Product copyWith({
     String? id,
@@ -31,5 +34,17 @@ class Product {
       imageUrl: imageUrl ?? this.imageUrl,
       isFavorite: isFavorite ?? this.isFavorite,
     );
+  }
+
+  set isFavorite(bool newValue) {
+    _isFavorite.value = newValue;
+  }
+
+  bool get isFavorite {
+    return _isFavorite.value;
+  }
+
+  ValueNotifier<bool> get isFavoriteListenable {
+    return _isFavorite;
   }
 }
